5144f23fb487b9da7af3abdd18f3656f59586325
Antonio Marquez Barrios <m.b.antonio898@gmail.com>
2024-04-19 13:34:22 +0200

    Creación Método Suma

--- src/modelo/Calculadora.java
+++ src/modelo/Calculadora.java
@@ -1,11 +1,15 @@
 package modelo;
 
 public class Calculadora {
-
+	private int mr;
 	public Calculadora() {
 		// TODO Auto-generated constructor stub
+		mr=0;
 	}
-	
+	public int suma(int a, int b) {
+		mr = a+b;
+		return mr;
+	}
 	
 	
 }
