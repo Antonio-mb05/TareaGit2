7bdea09aeff830a8c9d723a962e0994ac7c5a887
Antonio Marquez Barrios <m.b.antonio898@gmail.com>
2024-04-19 13:35:12 +0200

    Revert "Creación Método Suma"
    This reverts commit 5144f23fb487b9da7af3abdd18f3656f59586325.

--- src/modelo/Calculadora.java
+++ src/modelo/Calculadora.java
@@ -1,15 +1,11 @@
 package modelo;
 
 public class Calculadora {
-	private int mr;
+
 	public Calculadora() {
 		// TODO Auto-generated constructor stub
-		mr=0;
 	}
-	public int suma(int a, int b) {
-		mr = a+b;
-		return mr;
-	}
+	
 	
 	
 }
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,15 +1,10 @@
 package vista;
 
-import modelo.Calculadora;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Estudio Git");
-		Calculadora calc = new Calculadora();
-		
-		System.out.println(calc.suma(1, 2));
 
 	}
 
